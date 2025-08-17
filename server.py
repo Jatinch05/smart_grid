923e8cfdb9e4e27f7d501239fb1c2e28fb0e4ae8
Chigurubattula Jatin <125136867+Jatinch05@users.noreply.github.com>
2025-08-17 15:21:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import paho.mqtt.client as mqtt
 
 # MQTT Broker settings
-broker = "e75df26b78d24d67aa8fcc75770584f1.s1.eu.hivemq.cloud"
+broker = "enter your broker url"
 port = 8883  # SSL port
-topic = "esp32/test"
-username = "Jatin"
-password = "Jatinch@2005"
+topic = "enter your topic name"
+username = "enter your username"
+password = "enter your password"
 
 # Path to the CA certificate file
-ca_cert_path = "broker.pem"
+ca_cert_path = "path to your certificate"
 # Define what happens on connection
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -26,7 +26,7 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 # Enable TLS and set the CA certificate for broker verification
-client.tls_set(ca_certs=ca_cert_path)  # Ensure the path to your root CA certificate file is correct
+client.tls_set(ca_certs="path to your certificate")  # Ensure the path to your root CA certificate file is correct
 
 # Connect and run the client loop
 client.connect(broker, port)
